14a46a0403586c6f59a2ee58bf4ee1beccfdf282
houssein ashrafipoor <ashrafipoor@gmail.com>
2022-03-13 23:18:57 +0100

    Add Main Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <aside className='w-1/6 float-left  border-r-gray-700 border-r h-screen '></aside>
+      <main className='w-5/6 float-left h-screen'></main>
     </div>
   );
 }
